50ce8866cd1e48a23dd960da11b53804294da7bb
Aeydyry <bravoalexandre@yahoo.com>
2025-05-15 12:02:21 +0200

    maj

--- scripts/DQ_eval.sh
+++ scripts/DQ_eval.sh
@@ -1,7 +1,9 @@
-checkpoint=$1
+coco_path=$1
+checkpoint=$2
+
 python main_aitod.py \
   --output_dir logs/DQ_eval \
-	-c config/DQ_5scale.py --coco_path /nfs/home/hoiliu/Datasets/aitod \
+	-c config/DQ_5scale.py --coco_path $coco_path \
 	--eval --resume $checkpoint \
 	--options dn_scalar=100 embed_init_tgt=False \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
