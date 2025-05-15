b6bba033d90e5cce3461e05949d3386e6c519e36
Aeydyry <bravoalexandre@yahoo.com>
2025-05-15 10:34:51 +0200

    maj

--- main_aitod.py
+++ main_aitod.py
@@ -211,6 +211,7 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu', weights_only=False)
+        print(checkpoint['model'])
         model_without_ddp.load_state_dict(checkpoint['model'])
         if args.use_ema:
             if 'ema_model' in checkpoint:
