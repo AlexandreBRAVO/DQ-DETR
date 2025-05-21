57d65e5088e44a1b0e0e03b87793689dcd6edfcf
Aeydyry <bravoalexandre@yahoo.com>
2025-05-21 10:49:05 +0200

    maj

--- scripts/DQ_train.sh
+++ scripts/DQ_train.sh
@@ -1,6 +1,6 @@
 coco_path=$1
 srun python main_aitod.py \
-	--output_dir logs/LEVIR -c config/DQ_5scale.py --coco_path $coco_path \
+	--output_dir logs/LEVIR -c config/DQ_train.py --coco_path $coco_path \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
 	dn_box_noise_scale=1.0
